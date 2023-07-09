658bc8083ee6917a3a480641992a7a967a774be2
prasanna89654 <poudellprasanna789@gmail.com>
2023-07-09 15:53:46 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express JS on Vercel");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong 🏓");
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err, res) => {
+  if (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  } else {
+    console.log("[INFO] Server Running on port:", port);
+  }
+});
